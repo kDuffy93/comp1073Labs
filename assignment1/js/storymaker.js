0cd3969d40a454c213e015fdf738862f67f65a91
Kyle Duffy <kyleduffy83@gmail.com>
2022-01-27 17:36:56 -0500

    styling, logic
    more styling and no voice logic

--- assignment1/js/storymaker.js
+++ assignment1/js/storymaker.js
@@ -36,15 +36,19 @@ function populateVoiceList() {
     for (i = 0; i < voices.length; i++) {
         var option = document.createElement('option');
         option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
-
-        if (voices[i].default) {
-            option.textContent += ' -- DEFAULT';
-        }
+       
 
         option.setAttribute('data-lang', voices[i].lang);
         option.setAttribute('data-name', voices[i].name);
         voiceSelect.appendChild(option);
-    }
+     }
+     if(voices.length === 0){
+         var option = document.createElement('option');
+         option.textContent = 'No Available Voices';
+         option.setAttribute('data-lang', 'no-Lang');
+         option.setAttribute('data-name', 'no-Name');
+         voiceSelect.appendChild(option);
+     }
 }
 
 let setCurrentWords = (which, array,index) => 
