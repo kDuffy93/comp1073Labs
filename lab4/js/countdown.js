a0708dfda689a457e723d601c8bbba6bfe977fc6
Kyle Duffy <kyleduffy83@gmail.com>
2022-02-08 13:26:00 -0500

    styling and height Multiplier

--- lab4/js/countdown.js
+++ lab4/js/countdown.js
@@ -140,45 +140,46 @@ let increaseRocketHeights = async(iteration) => {
     return new Promise(resolve => {
         console.log(iteration);
         for (let i = 0; i < 8; i++) {
-            let randomAmount = (getRandomNumBetween(.1 * iteration + 1, 10 * iteration + 1) / getRandomNumBetween(250, 1500 + i));
+            let randomAmount = (getRandomNumBetween(.1 * iteration + 1, 10 * iteration + 1) / getRandomNumBetween(250, 750 + i));
             console.log(`randomAmount${i}:${randomAmount}`);
             if (randomAmount < 0) randomAmount *= (-1);
             console.log(`randomAmount${i}Neg:${randomAmount}`);
+            let heightMultiplier = .861875;
             switch (i) {
                 case 0:
                     rocket1Height += randomAmount;
                     console.log(rocket7Height);
-                    rocket1.style.transform = `translate3d(0, -${Number(rocket1Height) * .8875}vh, 0)`;
+                    rocket1.style.transform = `translate3d(0, -${Number(rocket1Height) * heightMultiplier}vh, 0)`;
                     break;
                 case 1:
                     rocket2Height += randomAmount;
                     console.log(rocket2Height);
-                    rocket2.style.transform = `translate3d(0, -${rocket2Height * .8875}vh, 0)`;
+                    rocket2.style.transform = `translate3d(0, -${rocket2Height * heightMultiplier}vh, 0)`;
                     break;
                 case 2:
                     rocket3Height += randomAmount;
                     console.log(rocket3Height);
-                    rocket3.style.transform = `translate3d(0, -${rocket3Height * .8875}vh, 0)`;
+                    rocket3.style.transform = `translate3d(0, -${rocket3Height * heightMultiplier}vh, 0)`;
                     break;
                 case 3:
                     rocket4Height += randomAmount;
                     console.log(rocket4Height);
-                    rocket4.style.transform = `translate3d(0, -${rocket4Height * .8875}vh, 0)`;
+                    rocket4.style.transform = `translate3d(0, -${rocket4Height * heightMultiplier}vh, 0)`;
                     break;
                 case 4:
                     rocket5Height += randomAmount;
                     console.log(rocket5Height);
-                    rocket5.style.transform = `translate3d(0, -${rocket5Height * .8875}vh, 0)`;
+                    rocket5.style.transform = `translate3d(0, -${rocket5Height * heightMultiplier}vh, 0)`;
                     break;
                 case 5:
                     rocket6Height += randomAmount;
                     console.log(rocket6Height);
-                    rocket6.style.transform = `translate3d(0, -${rocket6Height * .8875}vh, 0)`;
+                    rocket6.style.transform = `translate3d(0, -${rocket6Height * heightMultiplier}vh, 0)`;
                     break;
                 case 6:
                     rocket7Height += randomAmount;
                     console.log(rocket7Height);
-                    rocket7.style.transform = `translate3d(0, -${rocket7Height * .8875}vh, 0)`;
+                    rocket7.style.transform = `translate3d(0, -${rocket7Height * heightMultiplier}vh, 0)`;
                     break;
                 default:
                     console.log(`default from switch`);
@@ -456,14 +457,14 @@ and remove it below**
     if (resetSelection) {
         resetSelection = false;
         resetAllSelections();
-
+        currentlySelectedRockets = [];
     }
 
 
     let tempEle = document.getElementById(id);
     switch (playerNumber) {
         case 1:
-            currentlySelectedRockets = [];
+
             tempEle.classList.add(`selectedP${playerNumber}`);
             currentlySelectedRockets.push(id);
             if (playerCount == 1) {
