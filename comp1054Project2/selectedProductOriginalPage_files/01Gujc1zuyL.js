8a7a8eaa84417dbeecf77e15d622358407360eb9
Kyle Duffy <kyleduffy83@gmail.com>
2022-04-22 00:17:46 -0400

    Options section basic styling

---
+++ comp1054Project2/selectedProductOriginalPage_files/01Gujc1zuyL.js
@@ -0,0 +1,4 @@
+(function(c){var b=window.AmazonUIPageJS||window.P,d=b._namespace||b.attributeErrors,a=d?d("DetailPageBylineInfoAssets@core","DetailPageBylineInfo"):b;a.guardFatal?a.guardFatal(c)(a,window):a.execute(function(){c(a,window)})})(function(c,b,d){});
+/* ******** */
+(function(c){var b=window.AmazonUIPageJS||window.P,d=b._namespace||b.attributeErrors,a=d?d("SocialShareWidgetAUI","AmazonUI"):b;a.guardFatal?a.guardFatal(c)(a,window):a.execute(function(){c(a,window)})})(function(c,b,d){});
+/* ******** */
